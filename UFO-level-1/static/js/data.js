ea371de3d6440dc13cde7ac7058d67c11c6beda7
Dan Cusick <dan.cusick@gmail.com>
2021-05-25 22:12:18 -0500

    Fixed the data.

--- UFO-level-1/static/js/data.js
+++ UFO-level-1/static/js/data.js
@@ -4,7 +4,7 @@ var data = [{
     state: "il",
     country: "us",
     shape: "tesla",
-    durationMinutes: "5 mins.",
+    durationMinutes: "5 nanoseconds",
     comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
   },
   {
